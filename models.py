79f66de0f80a69479f537770cf85edb6f371ed3f
KFOLW <kfolweiler@live.com>
2017-01-07 21:07:01 -0500

    added Log class

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
-
+from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship 
 
 class Mouse(db.Model):
 	__tablename__ = 'mouse'
@@ -15,3 +17,23 @@ class Mouse(db.Model):
 
 	def  __repr__(self):
 		return '<id {}>'.format(self.id)
+
+
+class Log(db.Model):
+	__tablename__ = "logentry"
+
+	id = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.DateTime())
+	note = db.Column(db.String())
+	mouse_id = db.Column(db.Integer, ForeignKey('mouse.id'))
+
+	owner = relationship("Mouse", back_populates="logs")
+
+	def __init__(self, date, note):
+		self.date = date
+		self.note = note
+
+	def __repr__(self):
+		return '<id {}>'.format(self.id)
+
+Mouse.logs = relationship("Log", order_by="Log.id", back_populates="owner")
